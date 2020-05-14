72e71c8a2f4744b752fd2ce55d05e4d4da67dd69
vaetchegaray <vaetchegaray@uc.cl>
2020-05-14 15:55:18 -0400

    Delete gitlab file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/" + process.env.CI_PROJECT_NAME + "/"
-      : "/",
+  presets: ["@vue/cli-plugin-babel/preset"],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tarea03_integracion/'
+    : '/'
 };
