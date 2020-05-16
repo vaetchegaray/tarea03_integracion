18ee207b9de4f59a5ed2b00672e9a05987baf8de
vaetchegaray <vaetchegaray@uc.cl>
2020-05-16 00:01:15 -0400

    Fix data format

--- src/components/Graphic.vue
+++ src/components/Graphic.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="small">
-    <button @click="toggleConnnection">{{ button.state }}</button>
+    <button @click="toggleConnnection()">{{ button.state }}</button>
     <line-chart :chart-data="datacollection" id="mychart"></line-chart>
   </div>
 </template>
 
 <script>
-import LineChart from "../../LineChart.js";
+import LineChart from "./LineChart.js";
 import io from "socket.io-client";
 
 var socket = io("wss://le-18262636.bitzonte.com", { path: "/stocks" });
@@ -17,6 +17,7 @@ export default {
   components: {
     LineChart,
   },
+  props: {},
   data() {
     return {
       datacollection: {
@@ -37,8 +38,11 @@ export default {
       const time = new Date(fetchedata.time).toLocaleString("es-CL");
       const label = fetchedata.ticker;
       const value = fetchedata.value;
+      var new_stock = false;
+      var dataset_stock = null;
 
       if (!(label in stockValues)) {
+        new_stock = true;
         stockValues[label] = [
           {
             value: value,
@@ -46,19 +50,20 @@ export default {
             color: this.getColor(),
           },
         ];
+        dataset_stock = stockValues[label][0];
       } else {
         const stock = stockValues[fetchedata.ticker];
-        const check = stock.filter((stock) => stock.time == time);
+        const check = stock.filter((stock) => stock.time === time);
         if (check.length == 0) {
-          stock.push({
+          dataset_stock = {
             value: value,
             time: time,
             color: stock[0].color,
-          });
+          };
+          stock.push(dataset_stock);
         }
       }
-      this.constructDataset();
-      console.log(stockValues);
+      this.getDataset(new_stock, dataset_stock, label);
     },
     getColor() {
       var letters = "0123456789ABCDEF";
@@ -68,23 +73,26 @@ export default {
       }
       return color;
     },
-    constructDataset() {
-      const labels = [];
-      const datasets = [];
-      for (var label in stockValues) {
-        if (!(stockValues[label].time in labels)) {
-          labels.push(stockValues[label].time);
-          datasets.push({
-            data: stockValues[label].value,
-            label: label,
-            backgroundColor: stockValues[label].color,
-          });
+    getDataset(new_stock, dataset_stock, label) {
+      const labels = this.datacollection.labels;
+      const datasets = this.datacollection.datasets;
+
+      if (new_stock && dataset_stock) {
+        if (!labels.includes(dataset_stock.time)) {
+          labels.push(dataset_stock.time);
         }
+        datasets.push({
+          backgroundColor: dataset_stock.color,
+          label: label,
+          data: [dataset_stock.value],
+        });
+      } else if (dataset_stock) {
+        if (!labels.includes(dataset_stock.time)) {
+          labels.push(dataset_stock.time);
+        }
+        const to_push = datasets.find((stock) => stock.label === label);
+        to_push.data.push(dataset_stock.value);
       }
-      this.datacollection = {
-        labels: labels,
-        datasets: [],
-      };
     },
     getRealtimeData() {
       socket.on("UPDATE", (data) => {
